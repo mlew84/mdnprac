6db9788cda63464b432d2fca98531780beb3ef01
mlew84 <mblewis84@yahoo.com>
2021-09-26 01:59:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/tyler.jpg') {
+        myImage.setAttribute('src','images/roach.jpg');
+    } else{
+        myImage.setAttribute('src','images/tyler.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name',myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+    if(!localStorage.getItem('name')){
+        setUserName();
+    }   else{
+        let storedName = localStorage.getItem('name');
+        myHeading.textContent = 'Web Dev is COOL!, ' + storedName;
+    }
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
